15f145e9a79b326fa40393729e4ac2854f10608e
pranaylobo <42401887+pranaylobo@users.noreply.github.com>
2020-04-15 16:09:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const delay = require('delay');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
