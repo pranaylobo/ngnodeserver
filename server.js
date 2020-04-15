374e1aaa714c7d52f8b92b9b53e4a9a3884be41c
pranaylobo <42401887+pranaylobo@users.noreply.github.com>
2020-04-15 16:12:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const delay = require('delay');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://mngoconnection.web.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://mngoconnection.web.app');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
