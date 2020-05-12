b871fa5be3651ceb69756cdc993863c923ae286e
pranaylobo <42401887+pranaylobo@users.noreply.github.com>
2020-05-13 00:36:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,26 @@ const cors = require('cors')
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://Pranay:REDcherry%401@angulartest-wdzzt.gcp.mongodb.net/test?authSource=admin&replicaSet=Angulartest-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true";
 const fetch = require("node-fetch");
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const app = express();
 const delay = require('delay');
- var pass,email;
+var nodemailer = require('nodemailer');
 
+ var pass,email;
+ global.email1;
+ global.status1;
+ global.count=0;
+ global.count1=0;
+
+ global.checkuser;
+
+ global.res1;
+
+const router = express.Router();
+// app.use(cors());
+
+
+  
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
@@ -31,18 +46,15 @@ app.use(function (req, res, next) {
 });
 
 app.use(cors({
-  origin: ['https://mngoconnection.web.app/','https://mngoconnection.firebaseapp.com/'],
+  origin: ['https://mngoconnection.web.app','https://mngoconnection.web.app'],
   credentials: true
 }));
 
 app.use(cookieParser());
 app.use(session({
-  secret: 'ssshhhhh',
-  // create new redis store.
-  
+  secret: 'ssshhhhh',  
   saveUninitialized: true,
   resave: true,
-  // cookie: { maxAge: 3600000*6, secure: false,httpOnly: false }
 }));
 app.use(bodyParser.json());
 
@@ -75,24 +87,8 @@ let sess;
 
 app.post('/login',validatePayloadMiddleware,function(req,res)
 {
-//   console.log(req.session.email);
-//   if(req.session.email) {
-//       console.log("userin");
-//       res.json({
-//         message: true,
-//         pass1: "userin",
-//         email1: sess.email
-
-//       })
-//   }
-// // res.sendFile('index.html');
-//   else {
     console.log("se1", session);
     console.log("id",req.sessionID);
-    // sess = req.session;
-    // console.log("session",sess);
-    // console.log("session",sess.cookie.path);
-    
     console.log("reqhead",req.headers);
     console.log("sessemail",req.session);
     console.log(req.cookies);
@@ -175,24 +171,6 @@ app.post('/login',validatePayloadMiddleware,function(req,res)
     })
   }
 
-    // if(global.checkuser==false ){
-    //   res.json({
-    //     message:false,
-    //     pass1: "nopass",
-    //     email1: "noemail"
-
-    //   })
-    // }
-
-    // else{
-    //   req.session.email = req.body.email;
-    //   req.session.save();
-    //   res.json({
-    //     message:true,
-    //     pass1: pass,
-    //     email1: email
-    //   })
-    // }
   },1000 )
   
   
@@ -275,6 +253,317 @@ res.status(200).send({"message":"Data recieved"})
  });
 
 
+ app.post('/forgotpassword',function(req,res){
+  var transporter = nodemailer.createTransport({
+                 service: 'gmail',
+                 auth: {
+                     user: 'maheshkunder24@gmail.com',
+                     pass: 'alphabet@inc'
+                   },
+                   tls: {
+                     rejectUnauthorized: false
+                 }
+               });
+               
+               var mailOptions = {
+                 from: 'maheshkunder24@gmail.com',
+                 to: req.body.email,
+                 subject: 'Verification mail',
+                 text: 'http://localhost:4200/fpassword?email='+req.body.email+''
+               };
+               
+               transporter.sendMail(mailOptions, function(error, info){
+                 if (error) {
+                   console.log(error);
+                 } else {
+                   console.log('Email sent: ' + info.response);
+ 
+                   res.json({
+                     email: "sent"
+                   })
+                 }
+               });
+         
+   
+  })
+ 
+ 
+  app.post('/updatepassword',function(req,res){
+ 
+   MongoClient.connect(url, function(err, db) {
+     if (err) throw err;
+     var dbo = db.db("TestR");
+     console.log(req.body.password);
+     console.log(req.body.email);
+ 
+     dbo.collection("user").updateOne({email: req.body.email},{$set: {password: req.body.password}}, function(err, res) {
+       if (err) throw err;
+       console.log("1 document updated");
+       result = true;
+       db.close();
+ 
+       
+     });
+   });
+   
+   var transporter = nodemailer.createTransport({
+                  service: 'gmail',
+                  auth: {
+                      user: 'maheshkunder24@gmail.com',
+                      pass: 'alphabet@inc'
+                    },
+                    tls: {
+                      rejectUnauthorized: false
+                  }
+                });
+                
+                var mailOptions = {
+                  from: 'maheshkunder24@gmail.com',
+                  to: req.body.email,
+                  subject: 'Password Has Been Changed',
+                  text: 'Your password has been changed '
+                };
+                
+                transporter.sendMail(mailOptions, function(error, info){
+                  if (error) {
+                    console.log(error);
+                  } else {
+                    console.log('Email sent: ' + info.response);
+  
+                    res.json({
+                      email: "sent"
+                    })
+                  }
+                });
+          
+    
+   })
+  
+ 
+  app.post('/checkuserverification',function(req,res)
+  {
+   MongoClient.connect(url, function(err, db) {
+     if (err) throw err;
+     var dbo = db.db("TestR");
+     
+     dbo.collection("user").findOne({email:req.body.email},{status:true}, function(err, res) {
+       if (err) throw err;
+       console.log("res.email",res);
+       if(res === null)
+       {
+ global.checkuser=false
+       }
+       else
+       {
+         global.checkuser=true
+ 
+       }
+ 
+       
+ 
+ 
+      db.close();
+ 
+ 
+  });
+ 
+  (async () => {
+ 
+   await delay(1000);
+ 
+   // Executed 100 milliseconds later
+   console.log("check global",global.checkuser)
+ 
+   if(global.checkuser === true)
+   {
+ res.json({
+   message:true
+ })
+   }
+ 
+   if(global.checkuser === false)
+   {
+     res.json({
+       message:false
+     })
+   }
+ 
+ })();
+ 
+ 
+       
+     });
+ 
+ 
+  })
+ 
+  app.post('/checkuser',function(req,res)
+  {
+   MongoClient.connect(url, function(err, db) {
+     if (err) throw err;
+     var dbo = db.db("TestR");
+     
+     dbo.collection("user").findOne({email:req.body.email}, function(err, res) {
+       if (err) throw err;
+       console.log("res.email",res);
+       if(res === null)
+       {
+ global.checkuser=false
+       }
+       else
+       {
+         global.checkuser=true
+ 
+       }
+ 
+       
+ 
+ 
+      db.close();
+ 
+ 
+  });
+ 
+  (async () => {
+ 
+   await delay(1000);
+ 
+   // Executed 100 milliseconds later
+   console.log("check global",global.checkuser)
+ 
+   if(global.checkuser === true)
+   {
+ res.json({
+   message:true
+ })
+   }
+ 
+   if(global.checkuser === false)
+   {
+     res.json({
+       message:false
+     })
+   }
+ 
+ })();
+ 
+ 
+       
+     });
+ 
+ 
+  })
+ app.post('/enroll',function(req,res)
+ {
+   var email;
+     console.log(req.body);
+     console.log(req.body.name);
+     console.log(req.body.lname);
+     console.log(req.body.email);
+     console.log(req.body.password);
+ 
+     MongoClient.connect(url, function(err, db) {
+         if (err) throw err;
+         // Insert document in TestR
+         var dbo = db.db("TestR");
+         var myobj = { fname: req.body.name, lname:req.body.lname,email:req.body.email,password:req.body.password, status: false};
+       
+           dbo.collection("user").insertOne(myobj, function(err, res) {
+               if (err) throw err;
+               console.log("1 document inserted");
+               db.close();
+             });
+ 
+             // NOde Mailer
+ 
+             var transporter = nodemailer.createTransport({
+                 service: 'gmail',
+                 auth: {
+                     user: 'maheshkunder24@gmail.com',
+                     pass: 'alphabet@inc'
+                   },
+                   tls: {
+                     rejectUnauthorized: false
+                 }
+               });
+               
+               var mailOptions = {
+                 from: 'maheshkunder24@gmail.com',
+                 to: req.body.email,
+                 subject: 'Verification mail',
+                 text: 'http://localhost:4200/verified?email='+req.body.email+''
+ 
+               };
+               
+               transporter.sendMail(mailOptions, function(error, info){
+                 if (error) {
+                   console.log(error);
+                 } else {
+                   console.log('Email sent: ' + info.response);
+                 }
+               });
+         
+         
+       });
+     res.status(200).send({"message":"Data recieved"})
+ })
+ 
+ app.post('/update',function(req,res)
+ {
+ global.email1=req.body.email;
+   MongoClient.connect(url, function(err, db) {
+     if (err) throw err;
+     var dbo = db.db("TestR");
+     
+     dbo.collection("user").updateOne({email: req.body.email},{$set: {status: true}}, function(err, res) {
+       if (err) throw err;
+       console.log("1 document updated");
+       result = true;
+       db.close();
+ 
+       
+     });
+   });
+   
+ 
+   res.status(200).send({"message":"Data updated"})
+ 
+ });
+ 
+ app.get('/result', (req, res) => {
+ console.log("global email",global.email1);
+     MongoClient.connect(url, function(err, db) {
+       if (err) throw err;
+       var dbo = db.db("TestR");
+       
+       dbo.collection("user").findOne({email:global.email1}, function(err, res) {
+         if (err) throw err;
+         console.log(res["status"]);
+        global.status1 = res["status"];
+        db.close();
+ 
+        global.count=1;
+ 
+ 
+    });
+ 
+   
+         
+       });
+ 
+       if(count==1)
+       {
+         console.log("hello",global.status1);
+ 
+         res.json({
+           status:global.status1
+         })
+       }
+ 
+  
+ 
+     });     
+ 
 
  app.listen(PORT,function()
  {
