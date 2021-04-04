a630aab9cf07d9b13623f49a573f7bf0269e31c5
Pranay Lobo <42401887+pranaylobo@users.noreply.github.com>
2021-04-05 01:29:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const router = express.Router();
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'https://e-learning-portal-ce846.web.app');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
