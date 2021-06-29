4caa9d38ec5077a2986312c8c8d426d39387d82b
Pranay Lobo <42401887+pranaylobo@users.noreply.github.com>
2021-06-29 13:34:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const router = express.Router();
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
